d9267dfbcaf1c73101b71fabd71fc11bab743dcc
Phillip Choi <pchoi13370@gmail.com>
2019-01-04 00:36:57 -0800

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { render } from "react-dom";
+import StorePicker from "../src/components/StorePicker";
+import App from "../src/components/App";
+
+render(<App />, document.querySelector("#main"));
